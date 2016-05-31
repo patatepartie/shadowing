6e175822141d415438872b2e9220096f3c0337fd
Cyril Ledru <cyril.ledru@kvh.co.jp>
2016-06-01 08:21:19 +0900

    Script documentation for e-l-v

--- extract-label-values.sh
+++ extract-label-values.sh
@@ -1,5 +1,33 @@
 #!/usr/bin/env bash
 
+# Find and display label information from [Audacity]() project files (`.aup`).
+#
+# Given a file name:
+# * extract the start, end and title of each label found
+# * concatenate the file base name and title, to form a dialog coordinate
+# * display coordinates and timing to `stdout`.
+# The script works on a single file name, given as argument or through `stdin`.
+#
+# Example `2-1.aup` file:
+# <?xml version="1.0" standalone="no" ?>
+# <!DOCTYPE [...] >
+# <project xmlns=[...]
+#	[...]
+# 	<labeltrack name="Label Track" numlabels="10" height="73" minimized="0" isSelected="1">
+#     <label t="5.1725104681" t1="7.1418036101" title="1"/>
+#     <label t="9.9461767555" t1="13.3238884105" title="2"/>
+#     [...]
+#     <label t="74.8789684769" t1="78.9645313351" title="10"/>
+#   </labeltrack>
+# </project>
+#
+# Partial output for the previous file:
+# 2-1-1 5.1725104681 7.1418036101
+# 2-1-2 9.9461767555 13.3238884105
+# [...]
+# 2-1-10 74.8789684769 78.9645313351
+
+
 # TODO better name for this function
 function extract_label_values {
 	full_path="$1"
