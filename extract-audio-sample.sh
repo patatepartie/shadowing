2889b079e60050187eb4d23909904f465b4de34a
Cyril Ledru <cyril.ledru@kvh.co.jp>
2016-06-02 23:06:23 +0900

    Stop using the "coordinates" concept in e-a-s
    We can now use any number of positional arguments.
    To use them in the patterns, we need to refer to their respective position.

--- extract-audio-sample.sh
+++ extract-audio-sample.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash
 
-# $0 -s 9.9461767555 -e 13.3238884105 -d 2-1-1 -id Audio -ip '*_Unit_${unit}_-_Section_${section}.mp3' \
-#    -c mp3 -od Extracts -op '${out_dir}/${unit}-${section}-${dialog}.${codec}'
-
 function print_help {
-	usage="$(basename "${prog_name}") [[infile options] -id indir]... [[outfile options] -od outdir]...  UNIT SECTION DIALOG
-	extract a sample of an audio file, and name it based on its coordinates: UNIT, SECTION and DIALOG.
-	The coordinates and the file path will be output.
+	usage="$(basename "${prog_name}") [[infile options] -id indir]... [[outfile options] -od outdir]... ARGS...
+	extract a sample of an audio file, and name it based on a pattern filed by ARGS.
+	The dash-separated arguments and the file path will be output.
+
+	Example: $(basename "${prog_name}") -p "*_$1_$2_$3.ogg" -P "$3-$2-$1" a 23 z
+	will read a file matching the glob pattern '*_a_23_z.ogg' and write a file named 'z-23-a.mp3'.
 
 where:
     -h  show this help text
-    -s  start time of the dialog to extract (in fractional seconds, default: beginning of input file)
-    -e  end time of the dialog to extract (in fractional seconds, default: end of input file)
-    -i  directory of the input shadowing audio file (default: 'inputs')
-    -p  pattern of the input shadowing audio file. It can use 'unit' and 'section' variables 
-        (default: '*_Unit_${unit}_-_Section_${section}.mp3')
-    -c  codec of the extracted dialog audio file (default: mp3)
-    -o  directory to output the dialog file (default: 'outputs')
-    -P  pattern of the extracted dialog audio file. 
-        It can use 'unit', 'section', 'dialog' and 'codec' variables.
-        (default: '${unit}-${section}-${dialog}.${codec}')
+    -s  start time of the sample to extract (in fractional seconds, default: beginning of input file)
+    -e  end time of the sample to extract (in fractional seconds, default: end of input file)
+    -i  directory of the input audio file (default: 'inputs')
+    -p  pattern of the input audio file. It can use any positional arguments 
+        (default: '*_Unit_${1}_-_Section_${2}.mp3')
+    -c  codec of the extracted sample audio file (default: mp3)
+    -o  directory to output the sample file to (default: 'outputs')
+    -P  pattern of the extracted sample audio file. 
+        It can use any positional arguments (default: '${1}-${2}-${3}.${codec}')
     -v  verbose mode, mainly for ffmpeg
     "
 	echo "${usage}"
@@ -34,10 +33,10 @@ function param_error {
 prog_name=${0}
 
 input_dir="inputs"
-input_pattern='*_Unit_${unit}_-_Section_${section}.mp3'
+input_pattern='*_Unit_${1}_-_Section_${2}.mp3'
 codec="mp3"
 output_dir="outputs"
-output_pattern='${unit}-${section}-${dialog}.${codec}'
+output_pattern='${1}-${2}-${3}.${codec}'
 
 # TODO use silent mode when debug is done (prefix by ":"")
 while getopts "hvs:e:i:p:c:o:P:" opt; do
@@ -80,14 +79,6 @@ while getopts "hvs:e:i:p:c:o:P:" opt; do
 done
 
 shift $((OPTIND-1))
-
-unit="${1}"
-section="${2}"
-dialog="${3}"
-
-if [ -z "${unit}" ] || [ -z "${section}" ] || [ -z "${dialog}" ]; then
-    param_error "3 positional parameters needs to be set: UNIT SECTION DIALOG."
-fi
 
 eval input_path="${input_dir}/${input_pattern}"
 eval output_path="${output_dir}/${output_pattern}"
