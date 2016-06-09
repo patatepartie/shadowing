df0f54cf9d8b5bd6532ba0d76b60dce86bd32b67
Cyril Ledru <cyril.ledru@kvh.co.jp>
2016-06-10 08:10:48 +0900

    Use a fixed pattern for output filename
    It pads the numbers on two digits with a leading 0.
    Output the file path for reference since we won't use the output in csv anymore.

--- extract-audio-sample.sh
+++ extract-audio-sample.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 function print_help {
-	usage="$(basename "${prog_name}") [[infile options] -id indir]... [[outfile options] -od outdir]... ARGS...
-	extract a sample of an audio file, and name it based on a pattern filed by ARGS.
+	usage="$(basename "${prog_name}") [[infile options] -i indir]... [[outfile options] -o outdir]... ARGS...
+	extract a sample of an audio file, and name it based on ARGS.
 	The dash-separated arguments and the file path will be output.
 
-	Example: $(basename "${prog_name}") -p "*_$1_$2_$3.ogg" -P "$3-$2-$1" a 23 z
-	will read a file matching the glob pattern '*_a_23_z.ogg' and write a file named 'z-23-a.mp3'.
+	Example: $(basename "${prog_name}") -p "*_$1_$2_$3.ogg" 5 23 2
+	will read a file matching the glob pattern '*_a_23_z.ogg' and write a file named '5-23-2.mp3'.
 
 where:
     -h  show this help text
@@ -17,8 +17,6 @@ where:
         (default: '*_Unit_${1}_-_Section_${2}.mp3')
     -c  codec of the extracted sample audio file (default: mp3)
     -o  directory to output the sample file to (default: 'outputs')
-    -P  pattern of the extracted sample audio file. 
-        It can use any positional arguments (default: '${1}-${2}-${3}.${codec}')
     -v  verbose mode, mainly for ffmpeg
     "
 	echo "${usage}"
@@ -36,9 +34,8 @@ input_dir="inputs"
 input_pattern='*_Unit_${1}_-_Section_${2}.mp3'
 codec="mp3"
 output_dir="outputs"
-output_pattern='${1}-${2}-${3}.${codec}'
 
-while getopts "hvs:e:i:p:c:o:P:" opt; do
+while getopts "hvs:e:i:p:c:o:" opt; do
 	case $opt in
 		h)
 			print_help
@@ -62,9 +59,6 @@ while getopts "hvs:e:i:p:c:o:P:" opt; do
 		o)
 			output_dir="${OPTARG}"
 			;;
-		P)
-			output_pattern="${OPTARG}"
-			;;
 		v)
 			verbose="true"
 			;;
@@ -80,7 +74,7 @@ done
 shift $((OPTIND-1))
 
 eval input_path="${input_dir}/${input_pattern}"
-eval output_file="${output_pattern}"
+printf -v output_file "%02d-%02d-%02d.%s" ${1} ${2} ${3} ${codec}
 output_path="${output_dir}/${output_file}"
 
 ffmpeg_cmd="ffmpeg -hide_banner -i ${input_path}"
@@ -100,4 +94,4 @@ ffmpeg_cmd="${ffmpeg_cmd} -acodec ${codec} ${output_path}"
 
 `${ffmpeg_cmd}`
 
-echo "${1}-${2}-${3},[sound:${output_file}]"
+echo "${output_file}"
